fd219a4004f94547b52fe4f442e442428da394a3
mbalazs00 <marko.balazs00@gmail.com>
2024-11-12 22:42:40 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,139 @@
+const uri = 'api/todoitems';
+let todos = [];
+
+function getItems() {
+  fetch(uri)
+    .then(response => response.json())
+    .then(data => _displayItems(data))
+    .catch(error => console.error('Unable to get items.', error));
+}
+
+function addItem() {
+  const addNameTextbox = document.getElementById('add-name');
+
+  const item = {
+    isComplete: false,
+    name: addNameTextbox.value.trim()
+  };
+
+  fetch(uri, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(response => response.json())
+    .then(() => {
+      getItems();
+      addNameTextbox.value = '';
+    })
+    .catch(error => console.error('Unable to add item.', error));
+}
+
+function deleteItem(id) {
+  fetch(`${uri}/${id}`, {
+    method: 'DELETE'
+  })
+  .then(() => getItems())
+  .catch(error => console.error('Unable to delete item.', error));
+}
+
+function displayEditForm(id) {
+  const item = todos.find(item => item.id === id);
+  
+  document.getElementById('edit-name').value = item.name;
+  document.getElementById('edit-id').value = item.id;
+  document.getElementById('edit-isComplete').checked = item.isComplete;
+  document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+  const itemId = document.getElementById('edit-id').value;
+  const item = {
+    id: parseInt(itemId, 10),
+    isComplete: document.getElementById('edit-isComplete').checked,
+    name: document.getElementById('edit-name').value.trim()
+  };
+
+  fetch(`${uri}/${itemId}`, {
+    method: 'PUT',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+  .then(() => getItems())
+  .catch(error => console.error('Unable to update item.', error));
+
+  closeInput();
+
+  return false;
+}
+
+function closeInput() {
+  document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+  const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+
+  document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+  const tBody = document.getElementById('todos');
+  tBody.innerHTML = '';
+
+  _displayCount(data.length);
+
+  data.forEach(item => {
+    
+    let isCompleteCheckbox = document.createElement('input');
+    isCompleteCheckbox.type = 'checkbox';
+    isCompleteCheckbox.disabled = true;
+    isCompleteCheckbox.checked = item.isComplete;
+
+    
+    var bsEdit = 'btn btn-success btn-sm mr-2';
+    var bsDelete = 'btn btn-danger btn-sm';
+    var iconEdit = '<i class="fas fa-edit"></i> Edit';
+    var iconDelete = '<i class="fas fa-trash-alt"></i> Delete';
+
+    let editButton = document.createElement('button');
+    editButton.style.backgroundColor = '#28a745';
+    editButton.style.color = '#fff';
+    editButton.innerHTML = iconEdit;
+    editButton.className = bsEdit;
+    
+    editButton.onclick = () => displayEditForm(item.id);
+   
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = iconDelete;
+    deleteButton.className = bsDelete;
+    deleteButton.style.backgroundColor = '#dc3545';
+    deleteButton.style.color = '#fff';  
+    deleteButton.onclick = () => deleteItem(item.id);
+
+    let tr = tBody.insertRow();
+    
+    let td1 = tr.insertCell(0);
+    td1.appendChild(isCompleteCheckbox);
+
+    let td2 = tr.insertCell(1);
+    let textNode = document.createTextNode(item.name);
+    td2.appendChild(textNode);
+
+    let td3 = tr.insertCell(2);
+    td3.appendChild(editButton);
+
+    let td4 = tr.insertCell(3);
+    td4.appendChild(deleteButton);
+
+     
+  });
+
+  todos = data;
+}
